4c627b05e533e55697455814c23ff8245b919153
cheryl-toh <cheryltqr2907@gmail.com>
2023-06-27 14:22:14 +0800

    comments added

--- src/BoyerMooreAlgorithm.java
+++ src/BoyerMooreAlgorithm.java
@@ -2,37 +2,48 @@ public class BoyerMooreAlgorithm {
 
     public int[] badCharacterShiftTable(char[] pattern){
 
+        //initialize shift table and pattern length
         int[] shiftTable = new int[256];
         int m = pattern.length;
 
+        //initialize shift table values to pattern length
         for (int i = 0; i < 256; i++) {
             shiftTable[i] = m;
         }
 
+        //update shift table with correct shift value
         for (int i = 0; i < m - 1; i++) {
             char c = pattern[i];
             shiftTable[c] = m - i - 1;
         }
 
-
         return shiftTable;
     }
 
     public int[] goodSuffixShiftTable(char[] pattern){
+
+        //initialize tables and pattern length
         int m = pattern.length;
         int[] borderTable = new int[m+1];
         int[] shiftTable = new int[m+1];
 
+        //initialize values in shift table to be 0
         for(int i=0; i<m + 1; i++){
             shiftTable[i] = 0;
         }
 
+        //preprocessing round 1
+        //fill out border table and shift table
         int i = m;
         int j = m + 1;
+
         borderTable[i] = j;
 
         while(i > 0){
+            //if pattern does not match with character in text, keep expanding to the right to find border of suffix
             while(j <= m && pattern[i - 1] != pattern[j - 1]){
+
+                //if mismatch is found, stop expanding border and update shift table
                 if (shiftTable[j] == 0) {
                     shiftTable[j] = j - i;
                 }
@@ -41,26 +52,29 @@ public class BoyerMooreAlgorithm {
                 j = borderTable[j];
             }
 
+            //if match is found means border is found, store the beginning value of border
+            //decrement i and j to continue search for border for the next character in pattern
             i--;
             j--;
             borderTable[i] = j;
         }
 
+        //preprocessing round 2
+        //fill out entire shift table
+        //initialize x and y
         int x;
         int y;
+
+        //make y the widest suffix border
         y = borderTable[0];
 
         for(x = 0; x <= m; x++)
         {
-        /* set the border position of the first character
-        of the pattern to all indices in array shift
-        having shift[i] = 0 */
+        //if shift table has any value that has not been filled, update with widest border value
             if(shiftTable[x] == 0)
                 shiftTable[x] = y;
 
-        /* suffix becomes shorter than bpos[0],
-        use the position of next widest border
-        as value of j */
+        //if suffix becomes shorter than the widest border, update y to become the next widest border of pattern
             if (x == y)
                 y = borderTable[y];
         }
@@ -69,32 +83,37 @@ public class BoyerMooreAlgorithm {
     }
 
     public void search(char[] text, char[] pattern){
+
+        //initialize shift values and length of pattern and text
         int s = 0;
         int j;
         int n = text.length;
         int m = pattern.length;
 
+        //preprocessing pattern
         int[] badCharacterShift = badCharacterShiftTable(pattern);
         int[] goodSuffixShift = goodSuffixShiftTable(pattern);
 
+        //start search
         while(s <= n-m) {
             j = m - 1;
 
-        /* Keep reducing index j of pattern while
-        characters of pattern and text are matching
-        at this shift s*/
-            while (j >= 0 && pattern[j] == text[s + j])
+            //if pattern character matches text character, decrement j until mismatch found or entire pattern matches
+            while (j >= 0 && pattern[j] == text[s + j]) {
                 j--;
+            }
 
-        /* If the pattern is present at the current shift,
-        then index j will become -1 after the above loop */
+            //if entire pattern matches, print shift value
             if (j < 0) {
                 System.out.println("pattern occurs at shift " + s);
+                //shift entire pattern to the right to continue searching
                 s += goodSuffixShift[0];
             } else {
-
+                //if mismatch is found, compute shift values for both heuristics
                 int badCharShiftValue = badCharacterShift[text[s+j] -m+1+j];
                 int goodSuffixShiftValue = goodSuffixShift[j+1];
+
+                //use shift value with the largest shift
                 s += Math.max(badCharShiftValue, goodSuffixShiftValue);
             }
         }
--- src/BoyerMooreExample.java
+++ src/BoyerMooreExample.java
@@ -3,13 +3,16 @@ import java.util.Arrays;
 public class BoyerMooreExample {
     public static void main(String[] args) {
 
+        //initialize text and pattern
         char[] text = "ABAAAABAACD".toCharArray();
         char[] pattern = "ABA".toCharArray();
 
+        //print information
         System.out.println("Text: " + Arrays.toString(text));
         System.out.println("Pattern: " + Arrays.toString(pattern));
         System.out.println();
 
+        //start search
         BoyerMooreAlgorithm BoyerMoore = new BoyerMooreAlgorithm();
         BoyerMoore.search(text, pattern);
     }
